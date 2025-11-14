56d9ef72bfac43f5cd370b821fc441ce5562c4b3
“aialabrrgn” <aialabarragan@gmail.com>
2025-11-14 14:22:23 +0100

    js termonado, fatan moodboards de fatalitas elfilla sonos freshs y bamfer

--- script.js
+++ script.js
@@ -1,28 +1,120 @@
 // portadas de proyectos
 
-const tituloCont = document.querySelector(".titulo__proyecto-lettre")
-const imgCont = document.querySelector(".proyectos__imgCont")
+// bamfer
+const tituloContBamfer = document.querySelector(".titulo__proyecto-bamfer")
+const imgContBamfer = document.querySelector(".proyectos__imgCont")
 
-
-// tituloCont.addEventListener("mouseover", () => {
-//     imgCont.classList.add('proyectos__imgCont-lettre');
-// });
-
-// tituloCont.addEventListener("mouseover", () => {
-//     imgCont.classList.toggle('proyectos__imgCont-lettre');
-// });
-// tituloCont.addEventListener("mouseout", () => {
-//     imgCont.classList.add('proyectos__imgCont');
-// });
-
-tituloCont.addEventListener('mouseenter', () => {
-    imgCont.classList.add('proyectos__imgCont-lettre');
+tituloContBamfer.addEventListener('mouseenter', () => {
+    imgContBamfer.classList.add('proyectos__imgCont-bamfer');
 });
-tituloCont.addEventListener('mouseleave', () => {
-    imgCont.classList.remove('proyectos__imgCont-lettre');
+tituloContBamfer.addEventListener('mouseleave', () => {
+    imgContBamfer.classList.remove('proyectos__imgCont-bamfer');
 });
 
-// const imgCont=document.querySelector(".proyectos__imgCont")
+// letrre
+const tituloContLettre = document.querySelector(".titulo__proyecto-lettre")
+const imgContLettre = document.querySelector(".proyectos__imgCont")
+
+tituloContLettre.addEventListener('mouseenter', () => {
+    imgContLettre.classList.add('proyectos__imgCont-lettre');
+});
+tituloContLettre.addEventListener('mouseleave', () => {
+    imgContLettre.classList.remove('proyectos__imgCont-lettre');
+});
+
+
+// sood
+const tituloContSood = document.querySelector(".titulo__proyecto-sood")
+const imgContSood = document.querySelector(".proyectos__imgCont")
+
+tituloContSood.addEventListener('mouseenter', () => {
+    imgContSood.classList.add('proyectos__imgCont-sood');
+});
+tituloContSood.addEventListener('mouseleave', () => {
+    imgContSood.classList.remove('proyectos__imgCont-sood');
+});
+
+// vision activa
+const tituloContActiva = document.querySelector(".titulo__proyecto-activa")
+const imgContActiva = document.querySelector(".proyectos__imgCont")
+
+tituloContActiva.addEventListener('mouseenter', () => {
+    imgContActiva.classList.add('proyectos__imgCont-activa');
+});
+tituloContActiva.addEventListener('mouseleave', () => {
+    imgContActiva.classList.remove('proyectos__imgCont-activa');
+});
+
+// olimpo
+const tituloContOlimpo = document.querySelector(".titulo__proyecto-olimpo")
+const imgContOlimpo = document.querySelector(".proyectos__imgCont")
+
+tituloContOlimpo.addEventListener('mouseenter', () => {
+    imgContOlimpo.classList.add('proyectos__imgCont-olimpo');
+});
+tituloContOlimpo.addEventListener('mouseleave', () => {
+    imgContOlimpo.classList.remove('proyectos__imgCont-olimpo');
+});
+
+// no gravity
+const tituloContGravity = document.querySelector(".titulo__proyecto-gravity")
+const imgContGravity = document.querySelector(".proyectos__imgCont")
+
+tituloContGravity.addEventListener('mouseenter', () => {
+    imgContGravity.classList.add('proyectos__imgCont-gravity');
+});
+tituloContGravity.addEventListener('mouseleave', () => {
+    imgContGravity.classList.remove('proyectos__imgCont-gravity');
+});
+
+// fatalitas
+const tituloContFatalitas = document.querySelector(".titulo__proyecto-fatalitas")
+const imgContFatalitas = document.querySelector(".proyectos__imgCont")
+
+tituloContFatalitas.addEventListener('mouseenter', () => {
+    imgContFatalitas.classList.add('proyectos__imgCont-fatalitas');
+});
+tituloContFatalitas.addEventListener('mouseleave', () => {
+    imgContFatalitas.classList.remove('proyectos__imgCont-fatalitas');
+});
+
+// elfilla
+const tituloContElfilla = document.querySelector(".titulo__proyecto-elfilla")
+const imgContElfilla = document.querySelector(".proyectos__imgCont")
+
+tituloContElfilla.addEventListener('mouseenter', () => {
+    imgContElfilla.classList.add('proyectos__imgCont-elfilla');
+});
+tituloContElfilla.addEventListener('mouseleave', () => {
+    imgContElfilla.classList.remove('proyectos__imgCont-elfilla');
+});
+
+// sonor
+const tituloContSonor = document.querySelector(".titulo__proyecto-sonor")
+const imgContSonor = document.querySelector(".proyectos__imgCont")
+
+tituloContSonor.addEventListener('mouseenter', () => {
+    imgContSonor.classList.add('proyectos__imgCont-sonor');
+});
+tituloContSonor.addEventListener('mouseleave', () => {
+    imgContSonor.classList.remove('proyectos__imgCont-sonor');
+});
+
+// fresh
+const tituloContFresh = document.querySelector(".titulo__proyecto-fresh")
+const imgContFresh = document.querySelector(".proyectos__imgCont")
+
+tituloContFresh.addEventListener('mouseenter', () => {
+    imgContFresh.classList.add('proyectos__imgCont-fresh');
+});
+tituloContFresh.addEventListener('mouseleave', () => {
+    imgContFresh.classList.remove('proyectos__imgCont-fresh');
+});
+
+
+
+
+
 
 
 
